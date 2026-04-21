e2002bf6d83bbbf6ea7c411619afe882ff903fa8
darlingmota <darlingmotta@gmail.com>
2026-04-21 08:25:38 +0100

    Cleaning raw data

---
+++ rawdatacsv.R
@@ -0,0 +1,9 @@
+# Data cleaning script
+
+# Yahoo Finance Global Markets Intelligence 2026
+
+# Transform raw data with 451 rows and 131 columns into clean data with 449 rows and 16 columns by removing redundant, correlated and irrelevant colums
+
+
+
+
